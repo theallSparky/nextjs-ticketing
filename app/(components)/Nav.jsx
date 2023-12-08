e7f35668b24a758e2af8860695cffcd7f9b13e62
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-08 14:54:28 -0600

    Built out nav component

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,5 +1,23 @@
+const { faTicket, faHome } = require("@fortawesome/free-solid-svg-icons");
+const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
+const { default: Link } = require("next/link");
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div>
+        <Link href="/">
+          <FontAwesomeIcon icon={faHome} className="icon" />
+        </Link>
+        <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
+        </Link>
+      </div>
+      <div>
+        <p className="text-default-text"></p>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
