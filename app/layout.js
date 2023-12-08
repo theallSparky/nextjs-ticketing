430b57ca5f210251980de316259883867ecdb3f1
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-08 14:32:39 -0600

    Help with icon jumpiness

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,10 @@ import Nav from "./(components)/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
