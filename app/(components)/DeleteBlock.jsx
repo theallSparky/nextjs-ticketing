604e4c6b6caac5be7450db53b672fe527b24043e
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-08 15:30:55 -0600

    Added icon and styling for DeleteBlock

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,5 +1,13 @@
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const DeleteBlock = () => {
-  return <div>DeleteBlock</div>;
+  return (
+    <FontAwesomeIcon
+      icon={faX}
+      className="text-red-400 hover:cursor-pointer hover:text-red-200"
+    />
+  );
 };
 
 export default DeleteBlock;
