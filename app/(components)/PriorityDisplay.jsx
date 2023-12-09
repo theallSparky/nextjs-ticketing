4ba2bbb720be73e2a1ba8cd2a26113655b9bb0e3
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-09 10:18:52 -0600

    Made and styled prioritydisplay

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PriorityDisplay = () => {
-  return <div>PriorityDisplay</div>;
+  return (
+    <div className="flex justify-start align-baseline">
+      <FontAwesomeIcon icon={faFire} className="text-red-400" />
+      <FontAwesomeIcon icon={faFire} className="text-red-400" />
+      <FontAwesomeIcon icon={faFire} className="text-red-400" />
+      <FontAwesomeIcon icon={faFire} className="text-red-400" />
+      <FontAwesomeIcon icon={faFire} className="text-red-400" />
+    </div>
+  );
 };
 
 export default PriorityDisplay;
