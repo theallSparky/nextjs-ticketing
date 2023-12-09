923f11db95a3c5f234ca5e072e940d3d096a0336
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-09 13:29:33 -0600

    Built out ticketForm

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -4,7 +4,32 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const TicketForm = () => {
-  return <div>TicketForm</div>;
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+  return (
+    <div className="flex justify-center">
+      <form>
+        <h3>Create your ticket</h3>
+        <label>Title</label>
+        <input
+          id="title"
+          name="name"
+          type="text"
+          onChange={handleChange}
+          required={true}
+          value={formData.title}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default TicketForm;
