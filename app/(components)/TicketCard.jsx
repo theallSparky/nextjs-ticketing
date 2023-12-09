2e9cd1b8b2d06efc718175904b7ec5b754a88367
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-09 10:58:40 -0600

    Got main base structure built out for ticketcard

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -17,8 +17,16 @@ const TicketCard = () => {
       <p className="whitespace-pre-wrap">
         this is the ticket desc. please do this ticket.
       </p>
-      <ProgressDisplay />
-      <StatusDisplay />
+      <div className="flex-grow"></div>
+      <div className="flex mt-2">
+        <div className="flex flex-col">
+          <p className="text-xs my-1">12/09/23 10:56AM</p>
+          <ProgressDisplay />
+        </div>
+        <div className="ml-auto flex items-end">
+          <StatusDisplay />
+        </div>
+      </div>
     </div>
   );
 };
