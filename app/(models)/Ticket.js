e9b5d7644a10bdd7b64456d89ad39c44f86fcd32
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-09 12:01:38 -0600

    Built out ticketSchema

---
+++ app/(models)/Ticket.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
