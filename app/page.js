b75e2b32b27b7ffb25c7b67854bb62c646fcbcc2
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-09 11:06:36 -0600

    Ticketcard page styling into grid formatting

--- app/page.js
+++ app/page.js
@@ -2,8 +2,13 @@ import TicketCard from "./(components)/TicketCard";
 
 const Dasboard = () => {
   return (
-    <div>
-      <TicketCard />
+    <div className=" p-5">
+      <div className="lg:grid grid-cols-2 cl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
     </div>
   );
 };
