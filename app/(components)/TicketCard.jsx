294a5807c559e597921ee44afc72a84fcaf8bf82
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-12-09 10:42:48 -0600

    Added a hr and ticket title to the ticket card

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -12,6 +12,8 @@ const TicketCard = () => {
           <DeleteBlock />
         </div>
       </div>
+      <h4>Ticket Title</h4>
+      <hr className="h-px border-0 bg-page mb-2" />
       <ProgressDisplay />
       <StatusDisplay />
     </div>
